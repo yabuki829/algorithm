51d6e2328f95ebf768eb9d484fd4318115e464b0
yabuki829 <syodai829@icloud.com>
2022-12-25 21:52:06 +0900

    簡単な動的計画法

--- other/DP.py
+++ other/DP.py
@@ -52,3 +52,96 @@ for i in range(1,N+1):
       
   
 print(dp[N][W])
+
+
+
+
+# 一番簡単な例題
+# https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_A&lang=jp
+# 問題　コイン
+#　額面がC1,C2,..., Cm　円の M種類のコインを使って、 N 円を支払うときの、コインの最小の枚数を求めて下さい。
+# 各額面のコインは何度でも使用することができます。
+
+# 6 15
+# 1 2 7 8 12 50
+
+
+# 今回の問題は,1,2,7,8,12,50を使って「15」を作る。
+M=6
+N=15
+conins = [1,2,7,8,12,50]
+
+opt = [float("inf")]*(N+1)
+# opt[0] = 0
+# opt[1] = 1
+# opt[2] = 1
+# opt[3] = 2 [1,2]
+# opt[4] = 2 [2,2]
+# opt[5] = 3 [1,2,2]
+
+opt[0] = 0
+# aは1~Nまで
+
+for a in range(1,N+1):
+  for c in conins:
+    if a >= c:
+      opt[a] = min(opt[a-c]+1,opt[a]) 
+
+if opt[N] == float("inf"):
+  print("作成できませんでした")
+else:
+  print(opt[N])
+
+
+
+# 足場の問題
+# https://atcoder.jp/contests/dp/tasks/dp_a
+
+# 4
+# 10 30 40 20
+# 足場 i にいるとき、足場 i+1 または i+2 へジャンプする。 このとき、ジャンプ先の足場を j とすると、コスト | hi - hj | を支払う
+
+
+# 今回の問題は,i番目の足場に行く最小コストを求める
+
+# 足場i+1に移動する
+# 足場i+2に移動する
+N = int(input())
+cost = [float("inf")]*(N)
+
+h = list(map(int,input().split()))
+cost[0] = 0
+
+for i in range(1,N):
+  # 現在の足場がiだとすると
+  # (1) 足場i-1までのコスト + 足場i-1から現在の足場iに移動するコスト
+  # (2) 足場i-2までのコスト + 足場i-2から現在の足場iに移動するコスト
+  # print("現在の足場",i)
+  a = cost[i-1]+abs(h[i]-h[i-1])
+  b = cost[i-2]+abs(h[i]-h[i-2])
+
+  cost[i] =  min(a,b)
+
+print(cost[-1])
+
+
+# 類問題
+# https://atcoder.jp/contests/dp/tasks/dp_b
+N,K = map(int,input().split())
+cost = [float("inf")]*(N)
+
+h = list(map(int,input().split()))
+cost[0] = 0
+
+for i in range(1,N):
+  # 1~Kまでの足場の移動について考える
+  for k in range(1,K+1):
+    # kが足場を超えないようにする
+    if k < len(h):
+    # k個前までの足場のコスト+k個前から現在の足場に移動するコスト
+      cost[i] = min(cost[i],cost[i-k]+abs(h[i]-h[i-k]))
+    
+    
+print(cost[-1])
+
+print(max(1,2))
